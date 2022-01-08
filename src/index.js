e021f95aaee3a395530062a3c4b94238faf7ab27
starwalker00 <aaronwalkerup@gmail.com>
2022-01-08 11:37:59 +0100

    graphql call to api

--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
+import { request, gql } from 'graphql-request';
+const dataFetchIntervalInMs = 1 * 2 * 1000;;
+const auctionNumberPerCall = 2;
+
+setInterval(() => {
+    const now = new Date();
+    console.log(now.toLocaleString())
+    callToSorareApi(auctionNumberPerCall).catch((error) => console.error(error))
+}, dataFetchIntervalInMs)
+
+async function callToSorareApi(num) {
+    const endpoint = 'https://api.sorare.com/graphql'
+    const query = gql`
+  query ListSoonEndingEnglishAuctions {
+    transferMarket {
+      englishAuctions(first: ${num.toString()}) {
+        nodes {
+          slug
+          currentPrice
+          startDate
+          endDate
+          cards {
+            slug
+            player{
+              age
+            }
+            name
+            rarity
+            priceRange{
+              min
+              max
+            }
+          }
+        }
+      }
+    }
+  }
+  `
+    const data = await request(endpoint, query)
+    console.log(JSON.stringify(data, undefined, 2))
+}
+
+
 const helloWorld = () => console.log('Hello World!');
 
 helloWorld();
+
